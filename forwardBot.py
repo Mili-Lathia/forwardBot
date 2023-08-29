9488bff18ab617a9c0f0f4cc0ddaa26c12b37cf7
Mili-Lathia <70016337+Mili-Lathia@users.noreply.github.com>
2023-08-29 12:00:34 +0530

    Update forwardBot.py

--- forwardBot.py
+++ forwardBot.py
@@ -1,1 +1,20 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
 print("heloooooo")
+async def start(update: Update, context: CallbackContext):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('6625229731:AAE1tVY77K8mugzLpA33K522xtyeG1jz2Tw').build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+
+    application.run_polling()
